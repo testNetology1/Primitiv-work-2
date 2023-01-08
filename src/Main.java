59eafb20b0fe489f528afaae95f4dce344cca07b
Albina <latipova.alb@gmail.com>
2023-01-08 22:21:59 +0500

    primitiv2

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int remains = 100;
+        int check = 100;
+        int deposit = 1100;
+        int doorstep = 1000;
+        int bonus ;
+        if (deposit > doorstep) ;
+        {
+            bonus = deposit / 100;
+        }
+
+        {
+            System.out.println("итоговая сумма" + check * 2 + "бонусов 0");
+            System.out.println(deposit + check + bonus +"итоговая сумма");
+            System.out.println("начисленно бонусов" + bonus);
+        }
+    }
+}
+
